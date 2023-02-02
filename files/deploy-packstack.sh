47ea08637ceeadc388a3e2c7442695573381f2b4
Evgeny Slutsky <eslutsky@redhat.com>
2023-02-02 14:52:25 +0200

    manually rediscover hosts after packstack installed
    Signed-off-by: Evgeny Slutsky <eslutsky@redhat.com>

--- files/deploy-packstack.sh
+++ files/deploy-packstack.sh
@@ -2,3 +2,6 @@
 
 #set the correct IP address
 packstack --answer-file=/packstack.answer
+
+#run host rediscovery in openstack yoga due to https://bugs.launchpad.net/packstack/+bug/1673305
+nova-manage cell_v2 discover_hosts
